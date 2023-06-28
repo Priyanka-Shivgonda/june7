0794559b1962579dd50a2ef6db149b6b5f70c78d
Priyanka-Shivgonda <priyankashivgonda96@gmail.com>
2023-06-29 01:56:15 +0530

    Bag name add

---
+++ Bag.java
@@ -0,0 +1,90 @@
+class Student{
+	  
+          String name;
+	  	  Student(String name)
+	{
+		
+        System.out.println("Student  name is  " + name);
+	    
+    }  
+}
+  class StudentRunner{
+	public static void main(String[] args)
+	{
+		System.out.println("Student runner main method....");
+		Student  s1=new Student("Priya");
+		Student  s2=new Student("Amulya");
+		Student  s3=new Student("Arati");
+	    Student  s4=new Student("Minshad");
+		Student  s5=new Student("Ambika");
+		Student  s6=new Student("Sudha");
+		Student  s7=new Student("Rani");
+		Student  s8=new Student("Soumya");
+		Student  s9=new Student("Pooja");
+	    Student  s10=new Student("Usha");
+	    Student  s11=new Student("Shivaleela");
+	}
+}
+
+
+
+
+class Phone{
+	String name;
+	Phone(String name)
+	{
+		System.out.println("running the phone constructor:  "  +  name);
+	}
+}
+    
+	
+	class PhoneRunner{
+		public static void main(String[] args)
+		{
+			System.out.println("Phone runner constructor");
+		  
+		    Phone ph1=new Phone("realme");
+		    Phone ph2=new Phone("quality");
+		    Phone ph3=new Phone("price");
+		    Phone ph4=new Phone("size");
+		    Phone ph5=new Phone("ram");
+		    Phone ph6=new Phone("company");
+		    Phone ph7=new Phone("battery");
+			Phone ph8=new Phone("clor");
+		    Phone ph9=new Phone("height"); 
+			Phone ph10=new Phone("phonecover");
+			
+			
+		}
+	}
+		
+	class  Bag{
+		String name;
+		Bag(String name)
+		{'
+		System.out.println("running the bag constructor : " + name);
+		}
+		
+		
+	class BagRunner{
+		public ststic void  main(String[] args)
+		{
+			System.out.println("bag runner is main method...........");
+		     Bag b1=new Bag("color");
+			 Bag b2=new Bag("size");
+			 Bag b3=new Bag("price");
+			 Bag b4=new Bag("adidas");
+			 Bag b5=new Bag("handbag");
+			 Bag b6=new Bag("brand");
+			 Bag b7=new Bag("laptopbag");
+			 Bag b8=new Bag("quantity");
+			 Bag b9=new Bag("quality");
+			 Bag b10=new Bag("weigth");
+			 Bag b11=new Bag("bagkey");
+			 
+			 
+		}
+	}
+	
+		
+	
