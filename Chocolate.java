a884f4a6a01c5f2732872df7560cbed4b2c940de
Priyanka-Shivgonda <137877242+Priyanka-Shivgonda@users.noreply.github.com>
2023-06-29 11:39:32 +0530

    Add files via upload

---
+++ Chocolate.java
@@ -0,0 +1,28 @@
+class Chocolate{
+  String name;
+  Cocolate(String name)
+{
+	System.out.println("chocolate name is" +name);
+}
+}
+
+
+
+
+class ChocolateRunner{
+	public static void main(String[] args)
+	{
+		System.out.println("chocolate runner main method");
+		Chocolate c1=new Chocolate("dairy milk");
+		Chocolate c2=new Chocolate("snickers");
+		Chocolate c3=new Chocolate("mailk bar");
+		Chocolate c4=new Chocolate("kit kat");
+		Chocolate c5=new Chocolate("color");
+		Chocolate c6=new Chocolate("price");
+		Chocolate c7=new Chocolate("size");
+		Chocolate c8=new Chocolate("brand");
+		Chocolate c9=new Chocolate("cadbarrry");
+		Chocolate c10=new Chocolate("5 star");
+	}
+}
+		
